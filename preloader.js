99ff5de50435908d4cc9aba369c6d66425d64e20
joisemp <joise1124@gmail.com>
2020-12-18 17:48:33 +0530

    finished creating preloader and pending some work of about section

---
+++ preloader.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', () => {
+    const loader_bg = document.querySelector(".loader_bg");
+    loader_bg.classList.add("preload-finish")
+    const loader = document.querySelector(".loader");
+    loader.classList.add("preload-finish1")
+})
