60031fdd272e631eb9a89cfc63b934ba3d9c2954
Joise MP <joise1124@gmail.com>
2022-06-16 17:32:21 +0530

    Removed all files

--- Assets/Javascript/preloader.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('load', () => {
-    const loader_bg = document.querySelector(".loader_bg");
-    loader_bg.classList.add("preload-finish")
-    const loader = document.querySelector(".loader");
-    loader.classList.add("preload-finish1")
-})
