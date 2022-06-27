0eae0347d145fdac9170de2452cfdf55b37560bd
Joise MP <joise1124@gmail.com>
2022-06-27 13:15:20 +0530

    Created a new model for programming projects

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,35 @@
+from turtle import title
 from django.db import models
+import sys
+import PIL
+from django.core.files.uploadedfile import InMemoryUploadedFile
+from io import BytesIO
 
-# Create your models here.
+
+class ProgrammingProject(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='images/programming_projects/')
+    summary = models.TextField(null=True, blank=True)
+    link = models.CharField(null=True, blank=True, max_length=300)
+    
+    def save(self):
+        # Opening the uploaded image
+        im = PIL.Image.open(self.image)
+
+        output = BytesIO()
+
+        # Resize/modify the image
+        im = im.resize((1024, 768))
+
+        # after modifications, save it to the output
+        im.save(output, format='JPEG', quality=90)
+        output.seek(0)
+
+        # change the imagefield value to be the newley modifed image value
+        self.image = InMemoryUploadedFile(output, 'ImageField', "%s.jpg" % self.image.name.split('.')[0], 'image/jpeg',
+                                          sys.getsizeof(output), None)
+
+        super(ProgrammingProject, self).save()
+    
+    def __str__(self):
+        return(self.title)
