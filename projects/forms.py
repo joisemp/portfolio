c232e635416a8448e1b69d650fca80f9cec42c9b
Joise MP <joise1124@gmail.com>
2022-07-09 23:19:38 +0530

    Start over

--- projects/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Contact
-
-
-class ContactForm(forms.ModelForm):
-    class Meta:
-        model = Contact
-        fields = ('full_name', 'email_id', 'subject', 'message')
