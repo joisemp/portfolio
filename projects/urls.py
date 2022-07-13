3cffef2dd17c3f2c40d060289e7672d8154efca7
Joise MP <joise1124@gmail.com>
2022-07-13 13:49:41 +0530

    Added project list page

--- projects/urls.py
+++ projects/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('', views.HomePageView.as_view(), name='home-page'),
     path('about/', views.AboutPageView.as_view(), name='about-page'),
     path('contact/', views.ContactPageView.as_view(), name='contact-page'),
-    path('project/<int:pk>/', views.ProjectDetailView.as_view(),
+    path('projects/', views.ProjectListPageView.as_view(), name='project-list-page'),
+    path('projects/<int:pk>/', views.ProjectDetailView.as_view(),
          name='project-detail-page'),
 ]
