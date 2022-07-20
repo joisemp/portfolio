ee8bda9806001986908066208f82c11c14363c3e
Joise MP <joise1124@gmail.com>
2022-07-20 17:23:58 +0530

    added filter option in project list page

--- projects/urls.py
+++ projects/urls.py
@@ -8,7 +8,12 @@ urlpatterns = [
     path('', views.HomePageView.as_view(), name='home-page'),
     path('about/', views.AboutPageView.as_view(), name='about-page'),
     path('contact/', views.ContactPageView.as_view(), name='contact-page'),
-    path('projects/', views.ProjectListPageView.as_view(), name='project-list-page'),
+    path('projects/', views.ProjectListPageView.as_view(),
+         name='project-list-page'),
+    path('projects/filter=programming-projects/',
+         views.ProgrammingProjectFilterView.as_view(), name='programming-project-filter-page'),
+    path('projects/filter=design-projects/',
+         views.DesignProjectFilterView.as_view(), name='design-project-filter-page'),
     path('projects/<int:pk>/', views.ProjectDetailView.as_view(),
          name='project-detail-page'),
 ]
